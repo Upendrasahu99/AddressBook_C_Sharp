819ef46be51996a7855d8f03ac7424e764340660
Upendra Sahu <upendrasahu1199@gmail.com>
2023-07-25 14:49:33 +0530

    Added: Add contact

--- AddressBook/Contact.cs
+++ AddressBook/Contact.cs
@@ -6,5 +6,23 @@ namespace AddressBook
 {
     internal class Contact
     {
+        public string FirstName{get; set;}
+        public string LastName { get; set;}
+        public int Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+
+    class Add : Contact
+    {
+        LinkedList<Contact> contacts = new LinkedList<Contact>();
+
+        public void AddContact(Contact contact)
+        {
+            contacts.AddLast(contact);
+        }
     }
 }
